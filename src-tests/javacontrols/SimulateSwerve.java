4f95a9f841899ab373935637923c2a9665f77f05
Petey <pdotjpr@gmail.com>
2018-06-27 00:15:58 -0700

    added EJML

---
+++ src-tests/javacontrols/SimulateSwerve.java
@@ -0,0 +1,11 @@
+package javacontrols;
+import org.ejml.simple.SimpleMatrix;
+
+public class SimulateSwerve {
+    
+    public static void main(String[] args) {
+        SimpleMatrix eye = SimpleMatrix.identity(3);
+        System.out.println(eye.toString());
+    }
+
+}
